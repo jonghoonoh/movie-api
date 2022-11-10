aa447e7cb3feb37e094ed94b43e0983e3d5347ad
jonghoonoh <jho@codeit.kr>
2022-11-10 12:15:43 +0900

    Add error handling and validation

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-let reviews = require('./reviews.json');
 
 const app = express();
 
@@ -13,46 +12,65 @@ const Review = require('./models/review');
 app.get('/reviews', async (req, res) => {
   const count = Number(req.query.count) || 0;
   const order = req.query.order || 'createdAt';
-  let reviews;
-  reviews = await Review.find().limit(count).sort([[order, 'desc']]);
-  res.send(reviews);
+  try {
+    const reviews = await Review.find().limit(count).sort([[order, 'desc']]);
+    res.send(reviews);
+  } catch (e) {
+    res.status(500).send({ message: e.message });
+  }
 });
 
 app.post('/reviews', async (req, res) => {
-  const newReview = await Review.create(req.body);
-  res.send(newReview);
+  try {
+    const newReview = await Review.create(req.body);
+    res.send(newReview);
+  } catch (e) {
+    res.status(400).send({ message: e.message });
+  }
 });
 
 app.get('/reviews/:id', async (req, res) => {
-  const review = await Review.findById(req.params.id);
-  if (review) {
-    res.send(review);
-  } else {
-    res.status(404).send({ message: 'Review not found' });
+  try {
+    const review = await Review.findById(req.params.id);
+    if (review) {
+      res.send(review);
+    } else {
+      res.status(404).send({ message: 'Review not found' });
+    }
+  } catch (e) {
+    res.status(500).send({ message: e.message });
   }
 });
 
 app.put('/reviews/:id', async (req, res) => {
-  const review = await Review.findById(req.params.id);
-  if (review) {
-    const newInfo = req.body;
-    Object.keys(newInfo).forEach(key => {
-      review[key] = newInfo[key];
-    });
-    await review.save();
-    res.send(review);
-  } else {
-    res.status(404).send({ message: 'Review not found' });
+  try {
+    const review = await Review.findById(req.params.id);
+    if (review) {
+      const newInfo = req.body;
+      Object.keys(newInfo).forEach(key => {
+        review[key] = newInfo[key];
+      });
+      await review.save();
+      res.send(review);
+    } else {
+      res.status(404).send({ message: 'Review not found' });
+    }
+  } catch {
+    res.status(400).send({ message: e.message });
   }
 });
 
 app.delete('/reviews/:id', async (req, res) => {
-  const review = await Review.findById(req.params.id);
-  if (review) {
-    await review.remove();
-    res.sendStatus(200);
-  } else {
-    res.status(404).send({ message: 'Review not found' });
+  try {
+    const review = await Review.findById(req.params.id);
+    if (review) {
+      await review.remove();
+      res.sendStatus(200);
+    } else {
+      res.status(404).send({ message: 'Review not found' });
+    }
+  } catch (e) {
+    res.status(500).send({ message: e.message });
   }
 });
 
--- models/review.js
+++ models/review.js
@@ -4,14 +4,24 @@ const ReviewSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
+    maxLength: 40,
   },
   rating: {
     type: Number,
     require: true,
+    min: 1,
+    max: 5,
   },
   content: {
     type: String,
     required: true,
+    validate: {
+      validator: function(v) {
+        const words = v.split(); 
+        return words.length > 10;
+      },
+      message: () => 'Must exceed 10 words',
+    }
   },
 }, { 
   timestamps: true
