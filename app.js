a10a7710cb6892b37fe4d2bf1f2bd32eda7ec457
jonghoonoh <jho@codeit.kr>
2022-11-10 16:44:34 +0900

    Fix bug and use app.route()

--- app.js
+++ app.js
@@ -9,69 +9,72 @@ mongoose.connect('mongodb://localhost/reviews');
 
 const Review = require('./models/review');
 
-app.get('/reviews', async (req, res) => {
-  const count = Number(req.query.count) || 0;
-  const order = req.query.order || 'createdAt';
-  try {
-    const reviews = await Review.find().limit(count).sort([[order, 'desc']]);
-    res.send(reviews);
-  } catch (e) {
-    res.status(500).send({ message: e.message });
-  }
-});
-
-app.post('/reviews', async (req, res) => {
-  try {
-    const newReview = await Review.create(req.body);
-    res.send(newReview);
-  } catch (e) {
-    res.status(400).send({ message: e.message });
-  }
-});
-
-app.get('/reviews/:id', async (req, res) => {
-  try {
-    const review = await Review.findById(req.params.id);
-    if (review) {
-      res.send(review);
-    } else {
-      res.status(404).send({ message: 'Review not found' });
+app
+  .route('/reviews')
+  .get(async (req, res) => {
+    const count = Number(req.query.count) || 0;
+    const order = req.query.order || 'createdAt';
+    try {
+      const reviews = await Review.find()
+        .limit(count)
+        .sort([[order, 'desc']]);
+      res.send(reviews);
+    } catch (e) {
+      res.status(500).send({ message: e.message });
     }
-  } catch (e) {
-    res.status(500).send({ message: e.message });
-  }
-});
-
-app.put('/reviews/:id', async (req, res) => {
-  try {
-    const review = await Review.findById(req.params.id);
-    if (review) {
-      const newInfo = req.body;
-      Object.keys(newInfo).forEach(key => {
-        review[key] = newInfo[key];
-      });
-      await review.save();
-      res.send(review);
-    } else {
-      res.status(404).send({ message: 'Review not found' });
+  })
+  .post(async (req, res) => {
+    try {
+      const newReview = await Review.create(req.body);
+      res.send(newReview);
+    } catch (e) {
+      res.status(400).send({ message: e.message });
     }
-  } catch {
-    res.status(400).send({ message: e.message });
-  }
-});
+  });
 
-app.delete('/reviews/:id', async (req, res) => {
-  try {
-    const review = await Review.findById(req.params.id);
-    if (review) {
-      await review.remove();
-      res.sendStatus(200);
-    } else {
-      res.status(404).send({ message: 'Review not found' });
+app
+  .route('/reviews/:id')
+  .get(async (req, res) => {
+    try {
+      const review = await Review.findById(req.params.id);
+      if (review) {
+        res.send(review);
+      } else {
+        res.status(404).send({ message: 'Review not found' });
+      }
+    } catch (e) {
+      res.status(500).send({ message: e.message });
     }
-  } catch (e) {
-    res.status(500).send({ message: e.message });
-  }
-});
+  })
+  .put(async (req, res) => {
+    try {
+      const review = await Review.findById(req.params.id);
+      if (review) {
+        const newInfo = req.body;
+        Object.keys(newInfo).forEach((key) => {
+          review[key] = newInfo[key];
+        });
+        await review.save();
+        res.send(review);
+      } else {
+        res.status(404).send({ message: 'Review not found' });
+      }
+    } catch (e) {
+      res.status(400).send({ message: e.message });
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      const review = await Review.findById(req.params.id);
+      if (review) {
+        await review.remove();
+        res.sendStatus(200);
+      } else {
+        res.status(404).send({ message: 'Review not found' });
+      }
+    } catch (e) {
+      res.status(500).send({ message: e.message });
+    }
+  });
 
 app.listen(3000, () => console.log('Server Started'));
