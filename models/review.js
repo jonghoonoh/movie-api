87176491596abf13e07f01c9d29e5de078e9fd8a
jonghoonoh <jho@codeit.kr>
2022-11-11 15:37:50 +0900

    Fix validation

--- models/review.js
+++ models/review.js
@@ -9,7 +9,7 @@ const ReviewSchema = new mongoose.Schema({
   rating: {
     type: Number,
     require: true,
-    min: 1,
+    min: 0,
     max: 5,
   },
   content: {
@@ -17,10 +17,10 @@ const ReviewSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: function(v) {
-        const words = v.split(); 
-        return words.length > 10;
+        const words = v.split(' '); 
+        return words.length > 5;
       },
-      message: () => 'Must exceed 10 words',
+      message: () => 'Must exceed 5 words',
     }
   },
 }, { 
